bf7b9439787d4440feee1eaecb1e2422e1af7e45
Dennis Murphy <dennismurphyjr33@gmail.com>
2024-06-19 11:32:01 -0400

    ruff pre-commit

--- app.py
+++ app.py
@@ -1,27 +1,36 @@
 import pandas as pd
-from PIL import Image
 import streamlit as st
 from stqdm import stqdm
 
-from ovr_analysis.screenshot_reader import fix_batter_types, fix_pitcher_types, get_player_type, get_ovr_analysis_data, read_image
+from ovr_analysis.screenshot_reader import (
+    fix_batter_types,
+    fix_pitcher_types,
+    get_player_type,
+    get_ovr_analysis_data,
+    read_image,
+)
 
-st.title('9 Innings OVR Analysis')
-st.markdown('Upload screenshots from OVR Analysis stats and this tool will aggregate the data into a single table.')
-st.warning('This has only been tested with screenshots from an iPhone 13. Screenshots from other devices may or may not be compatible.')
+st.title("9 Innings OVR Analysis")
+st.markdown(
+    "Upload screenshots from OVR Analysis stats and this tool will aggregate the data into a single table."
+)
+st.warning(
+    "This has only been tested with screenshots from an iPhone 13. Screenshots from other devices may or may not be compatible."
+)
 
-files = st.file_uploader('OVR Analysis Screenshots', accept_multiple_files=True)
+files = st.file_uploader("OVR Analysis Screenshots", accept_multiple_files=True)
 
 pitcher_data = pd.DataFrame()
 batter_data = pd.DataFrame()
 
-process_screenshots = st.button('Process Screenshots')
+process_screenshots = st.button("Process Screenshots")
 if process_screenshots:
     for file in stqdm(files):
         img = read_image(file)
         data = get_ovr_analysis_data(img)
         player_type = get_player_type(img)
 
-        if player_type == 'batter':
+        if player_type == "batter":
             batter_data = pd.concat((batter_data, data))
             batter_data = batter_data.drop_duplicates()
         else:
@@ -30,21 +39,24 @@ if process_screenshots:
 
 if len(pitcher_data):
     pitcher_data = fix_pitcher_types(pitcher_data)
-    st.header('Pitcher Data')
+    st.header("Pitcher Data")
     pitcher_data
 
 if len(batter_data):
     batter_data = fix_batter_types(batter_data)
-    st.header('Batter Data')
+    st.header("Batter Data")
     batter_data
 
 if len(pitcher_data):
-    st.header('Average Pitcher Stats')
-    avg_pitcher_stats = pitcher_data.groupby('Player').mean().sort_values('ERA', ascending=True)
+    st.header("Average Pitcher Stats")
+    avg_pitcher_stats = (
+        pitcher_data.groupby("Player").mean().sort_values("ERA", ascending=True)
+    )
     avg_pitcher_stats
 
 if len(batter_data):
-    st.header('Average Batter Stats')
-    avg_batter_stats = batter_data.groupby('Player').mean().sort_values('OPS', ascending=False)
+    st.header("Average Batter Stats")
+    avg_batter_stats = (
+        batter_data.groupby("Player").mean().sort_values("OPS", ascending=False)
+    )
     avg_batter_stats
-    
--- ovr_analysis/screenshot_reader.py
+++ ovr_analysis/screenshot_reader.py
@@ -1,11 +1,10 @@
-from pathlib import Path
-
 import numpy as np
 from paddleocr import PaddleOCR
 import pandas as pd
 from PIL import Image
 
-ocr = PaddleOCR(use_angle_cls=False, lang='en', show_log=False)
+ocr = PaddleOCR(use_angle_cls=False, lang="en", show_log=False)
+
 
 def read_image(file):
     img = Image.open(file)
@@ -13,24 +12,27 @@ def read_image(file):
 
     return img
 
+
 def get_player_type(img):
     header_box = (290, 270, 2235, 310)
     header = img.crop(header_box)
     header_text = [text[1][0] for text in ocr.ocr(np.array(header), cls=False)[0]]
 
-    if 'IP' in header_text:
-        return 'pitcher'
-    return 'batter'
+    if "IP" in header_text:
+        return "pitcher"
+    return "batter"
+
 
 def get_row_bounds(start=0, end=7):
     row_height = 80
     rows = [400, 480, 560, 640, 720, 800, 880]
-    row_bounds = [(start, start+row_height) for start in rows]
+    row_bounds = [(start, start + row_height) for start in rows]
 
     return row_bounds[start:end]
 
+
 def get_col_bounds(player_type):
-    if player_type == 'batter':
+    if player_type == "batter":
         cols = [
             (290, 350),
             (650, 120),
@@ -42,7 +44,7 @@ def get_col_bounds(player_type):
             (1620, 160),
             (1800, 160),
             (1970, 150),
-            (2120, 115)
+            (2120, 115),
         ]
     else:
         cols = [
@@ -55,19 +57,44 @@ def get_col_bounds(player_type):
             (1590, 150),
             (1760, 150),
             (1930, 150),
-            (2105, 135)
+            (2105, 135),
         ]
-    col_bounds = [(start, start+end) for (start, end) in cols]
-    
+    col_bounds = [(start, start + end) for (start, end) in cols]
+
     return col_bounds
 
+
 def get_cols(player_type):
-    if player_type == 'batter':
-        cols = ['Player', 'RISP', 'H', 'HR%', 'AVG', 'OBP', 'SLG', 'OPS', 'R', 'RBI', 'SB']
+    if player_type == "batter":
+        cols = [
+            "Player",
+            "RISP",
+            "H",
+            "HR%",
+            "AVG",
+            "OBP",
+            "SLG",
+            "OPS",
+            "R",
+            "RBI",
+            "SB",
+        ]
     else:
-        cols = ['Player', 'W/L/SV', 'IP', 'ERA', 'WHIP', 'HR/9', 'SO', 'K/9', 'B/9', 'RA']
+        cols = [
+            "Player",
+            "W/L/SV",
+            "IP",
+            "ERA",
+            "WHIP",
+            "HR/9",
+            "SO",
+            "K/9",
+            "B/9",
+            "RA",
+        ]
 
     return cols
+
 
 def get_ovr_analysis_data(img, row_start=0, row_end=7):
     player_type = get_player_type(img)
@@ -88,39 +115,62 @@ def get_ovr_analysis_data(img, row_start=0, row_end=7):
 
     return ovr_analysis_data
 
+
 def fix_batter_types(batter_data):
-    batter_data = batter_data.astype({
-        'RISP': float,
-        'H': 'Int64',
-        'HR%': float,
-        'AVG': float,
-        'OBP': float,
-        'SLG': float,
-        'OPS': float,
-        'R': 'Int64',
-        'RBI': 'Int64',
-        'SB': 'Int64'
-    })
-    batter_data.Player = batter_data.Player.str.replace(' ', '')
+    batter_data = batter_data.astype(
+        {
+            "RISP": float,
+            "H": "Int64",
+            "HR%": float,
+            "AVG": float,
+            "OBP": float,
+            "SLG": float,
+            "OPS": float,
+            "R": "Int64",
+            "RBI": "Int64",
+            "SB": "Int64",
+        }
+    )
+    batter_data.Player = batter_data.Player.str.replace(" ", "")
 
     return batter_data
 
+
 def fix_pitcher_types(pitcher_data):
-    pitcher_data[['W', 'L', 'SV']] = pitcher_data['W/L/SV'].str.split('-').apply(pd.Series)
-    pitcher_data = pitcher_data[['Player', 'W', 'L', 'SV', 'IP', 'ERA', 'WHIP', 'HR/9', 'SO', 'K/9', 'B/9', 'RA']]
-    pitcher_data = pitcher_data.astype({
-        'W': 'Int64',
-        'L': 'Int64',
-        'SV': 'Int64',
-        'IP': float,
-        'ERA': float,
-        'WHIP': float,
-        'HR/9': float,
-        'SO': 'Int64',
-        'K/9': float,
-        'B/9': float,
-        'RA': 'Int64'
-    })
-    pitcher_data.Player = pitcher_data.Player.str.replace(' ', '')
+    pitcher_data[["W", "L", "SV"]] = (
+        pitcher_data["W/L/SV"].str.split("-").apply(pd.Series)
+    )
+    pitcher_data = pitcher_data[
+        [
+            "Player",
+            "W",
+            "L",
+            "SV",
+            "IP",
+            "ERA",
+            "WHIP",
+            "HR/9",
+            "SO",
+            "K/9",
+            "B/9",
+            "RA",
+        ]
+    ]
+    pitcher_data = pitcher_data.astype(
+        {
+            "W": "Int64",
+            "L": "Int64",
+            "SV": "Int64",
+            "IP": float,
+            "ERA": float,
+            "WHIP": float,
+            "HR/9": float,
+            "SO": "Int64",
+            "K/9": float,
+            "B/9": float,
+            "RA": "Int64",
+        }
+    )
+    pitcher_data.Player = pitcher_data.Player.str.replace(" ", "")
 
     return pitcher_data
