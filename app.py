cd793bb74034da489cc7da3d988639397f709ef8
Dennis Murphy <dennismurphyjr33@gmail.com>
2025-07-06 14:08:04 -0400

    Add 5 tool page to pages.

--- app.py
+++ app.py
@@ -5,11 +5,20 @@ st.title("9 Innings Tools")
 
 pages = st.navigation(
     [
-        st.Page("tools/stats_parser.py", title="Stats Parser", url_path="stats"),
+        st.Page(
+            "tools/stats_parser.py",
+            title="Stats Parser",
+            url_path="stats",
+        ),
         st.Page(
             "tools/ticket_probs.py",
             title="Blue/Green Success Chances",
             url_path="blue_green_chances",
+        ),
+        st.Page(
+            "tools/five_tool.py",
+            title='"Five Tool Player" Train Calculator',
+            url_path="five_tool",
         ),
     ]
 )
